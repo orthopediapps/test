2d178a8a8cc020c561c034a8e9caaf22db0d3831
orthopediapps <63400368+orthopediapps@users.noreply.github.com>
2020-04-14 13:23:01 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let capture;
+
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+   capture = createCapture(VIDEO);
+  capture.size = (windowWidth, windowHeight);
+ //capture.hide();
+  //disable default touch events for mobile
+ var el = document.getElementsByTagName("canvas")[0];
+  el.addEventListener("touchstart", pdefault, false);
+  el.addEventListener("touchend", pdefault, false);
+  el.addEventListener("touchcancel", pdefault, false);
+  el.addEventListener("touchleave", pdefault, false);
+  el.addEventListener("touchmove", pdefault, false);
+  
+  
+ 
+ 
+}
+
+
+
+function pdefault(e){
+  e.preventDefault()
+
+}
+function draw() {
+ background(0);
+
+  image(capture, 0, 0, 100, width * capture.height / capture.width);
+ 
+}
+
