ba5808a32dc0a9eaba75480e0057eec9a8dbe794
orthopediapps <63400368+orthopediapps@users.noreply.github.com>
2020-04-14 14:07:21 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,52 @@
-let capture;
+var m = {x:0, y:0};
+var touchX = 0;
+var touchY = 0;
+var touchIsDown;
+var value =0;
 
 
 function setup() {
- createCanvas(windowWidth, windowHeight);
-   capture = createCapture(VIDEO);
-  capture.size = (windowWidth, windowHeight);
- capture.hide();
+  createCanvas(windowWidth, windowHeight);
+  background(25, 150, 45);
+
+
   //disable default touch events for mobile
- var el = document.getElementsByTagName("canvas")[0];
+  var el = document.getElementsByTagName("canvas")[0];
   el.addEventListener("touchstart", pdefault, false);
   el.addEventListener("touchend", pdefault, false);
   el.addEventListener("touchcancel", pdefault, false);
   el.addEventListener("touchleave", pdefault, false);
   el.addEventListener("touchmove", pdefault, false);
-  
-  
- 
- 
 }
 
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 function pdefault(e){
   e.preventDefault()
-
 }
+
 function draw() {
- background(0);
-
- image(capture, 0, 0, windowWidth, windowHeight);
-stroke(255);
-  if (touchStart === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
- 
+  update();
+   fill(value)
+ellipse (100, 100, 40, 40);
  
 }
+
+function update(){
+  //normalize interaction
+  m.x = max(touchX, mouseX);
+  m.y = max(touchY, mouseY);
+  m.pressed = mouseIsPressed || touchIsDown;
+
+}
+
+function touchStarted(){
+   if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
+}
+ 
